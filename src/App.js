2e9dece948d369147bacf9e2ebba820af2fe0030
Kyesha <franciskyesha1@gmail.com>
2019-07-15 13:39:29 -0400

    initial app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Title from "./Components/Title";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const API_KEY = "f2eb8f96660c3ab047ca7842df116edf"
+class App extends React.Component{
+  getWeather = async () => {
+    const api_call = await fetch(`https://developers.zomato.com/api${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render() {
+    return (
+    <div>
+      <Title />
+      <Form />
+      <Weather />
+
+
+
     </div>
-  );
+    );
+  }
 }
+
+
+
+
 
 export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Form extends React. Component {
+  render() {
+    return(
+      <div>Are you Hungry?</div>
+    );
+  }
+}
+
+
+export default Form;
