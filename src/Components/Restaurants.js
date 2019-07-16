7425e9e3f8c32ff4bfe71375dc0c7119c94bfaba
Kyesha <franciskyesha1@gmail.com>
2019-07-16 15:00:01 -0400

    api call successfully console logging

--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -3,7 +3,8 @@ import React from "react";
 class Restaurants extends React.Component {
   render() {
     return(
-      <div>Lets get something to Grub on!</div>
+      <div className="container">
+      </div>
     );
   }
 }
